0736bd8b8035d0f1eec86a29f2753bea1f77a7e8
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-02-27 03:11:41 +0530

    used callBack cases

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 //import { Assignment1 } from "./components/Assignment1"
 
-import { Assignment1 } from "./components/useCallback/Assignment1"
+import { Assignment2 } from "./components/useCallback/Assignment2"
+
+//import { Assignment1 } from "./components/useCallback/Assignment1"
 
 //import { Assignment2 } from "./components/Assignment2"
 //import { Assignment3 } from "./components/useMemo/Assignment3"
@@ -12,7 +14,8 @@ function App() {
     {/*<Assignment1/>*/}
     {/*<Assignment2/>*/}
     {/*<Assignment3/>*/}
-    <Assignment1/>
+    {/*<Assignment1/>*/}
+    <Assignment2/>
     </div>
   )
 }
