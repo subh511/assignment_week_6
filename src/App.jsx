33dbaa0f0c697f0a3ac88fb474d4bf184be1092a
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-02-26 13:07:53 +0530

    intial push

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { Assignment1 } from "./components/Assignment1"
+//import { Assignment1 } from "./components/Assignment1"
+
+import { Assignment2 } from "./components/Assignment2"
 
 function App() {
 
   return (
     <div>
-    <Assignment1/>
+    {/*<Assignment1/>*/}
+    <Assignment2/>
     </div>
   )
 }
